41b75dae55f9f6ac0916b43ce3d49df528494f8f
John Lukach <84751182+jblukach@users.noreply.github.com>
2021-12-02 02:53:47 +0000

    init

---
+++ app.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+import os
+
+from aws_cdk import core as cdk
+
+from demystify.demystify_stack import DemystifyStack
+
+app = cdk.App()
+
+DemystifyStack(
+    app, 'DemystifyStack',
+    env = cdk.Environment(
+        account = os.getenv('CDK_DEFAULT_ACCOUNT'),
+        region = 'us-west-2'
+    ),
+    synthesizer = cdk.DefaultStackSynthesizer(
+        qualifier = '4n6ir'
+    )
+)
+
+cdk.Tags.of(app).add('demystify','demystify')
+
+app.synth()
