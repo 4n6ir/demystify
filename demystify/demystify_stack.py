0faa0036ee58504baa0a52dc6bf4febf975b8125
John Lukach <84751182+jblukach@users.noreply.github.com>
2021-12-04 01:59:35 +0000

    2.0.0

--- demystify/demystify_stack.py
+++ demystify/demystify_stack.py
@@ -1,9 +1,14 @@
 from aws_cdk import (
-    core as cdk,
+    Duration,
+    RemovalPolicy,
+    Stack,
 )
 
+from constructs import Construct
 
-class DemystifyStack(cdk.Stack):
+class DemystifyStack(Stack):
 
-    def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
+        
+        
