93bf8ba5e00efa70ab1366844c3a49d56f573e6d
John Lukach <84751182+jblukach@users.noreply.github.com>
2021-12-19 15:40:28 +0000

    2.2.0

--- demystify/demystify_stack.py
+++ demystify/demystify_stack.py
@@ -1,14 +1,19 @@
 from aws_cdk import (
-    Duration,
-    RemovalPolicy,
+    # Duration,
     Stack,
+    # aws_sqs as sqs,
 )
-
 from constructs import Construct
 
 class DemystifyStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-        
-        
+
+        # The code that defines your stack goes here
+
+        # example resource
+        # queue = sqs.Queue(
+        #     self, "DemystifyQueue",
+        #     visibility_timeout=Duration.seconds(300),
+        # )
